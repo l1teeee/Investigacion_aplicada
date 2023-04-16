927ae55f030318cfa2619fb3bc429720b35e5712
Kenayred <kenayred1.7.2@gmail.com>
2023-04-16 12:17:02 -0600

    Update index.php

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
   <html>
     <head>
-      <link rel="stylesheet" src="../resources/css/app.css" type="text/css">
+      <link rel="stylesheet" src="../resources/css/" type="text/css">
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
       <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
       <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
