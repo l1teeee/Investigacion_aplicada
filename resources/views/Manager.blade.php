71fbc8b6c3c5565d6dac14bf1cffab334e73eed8
l1teeee <89312593+l1teeee@users.noreply.github.com>
2023-04-16 13:46:20 -0600

    Update 9

--- resources/views/Manager.blade.php
+++ resources/views/Manager.blade.php
@@ -1,9 +1,8 @@
  
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
   <html>
     <head>
       <link rel="stylesheet" href="{{ asset('css/app.css') }}"/>
-      <link rel="stylesheet" src="css/app.css" type="text/css">
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
       <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
       <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
@@ -148,7 +147,7 @@
 
 
   </body>
-</html>
+</html> -->
 
 
  
