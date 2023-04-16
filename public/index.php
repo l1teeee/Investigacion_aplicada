48518ac5540dc9420175630d78bb64bbd6934ad4
l1teeee <89312593+l1teeee@users.noreply.github.com>
2023-04-16 11:12:27 -0600

    Update6

--- public/index.php
+++ public/index.php
@@ -3,25 +3,24 @@
   <html>
     <head>
       <link rel="stylesheet" src="../resources/css/app.css" type="text/css">
-      <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
       <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
       <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
     </head>
     <body style="">
 	<div class="tabla" >
-	<table class="table" >
+	<table class="table table-striped table-hover" >
   	<thead>
     <tr>
       <th scope="col">
         <div class="form-check">
-          <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
         </div>
       </th>
-      <th scope="col">Lorem</th>
-      <th scope="col">Ipsum</th>
-      <th scope="col">Dolor</th>
-      <th scope="col">Dolor</th>
-
+      <th scope="col">ID</th>
+      <th scope="col">Nombre</th>
+      <th scope="col">Apellido</th>
+      <th scope="col">Edad</th>
+      <th scope="col">Salario</th>
     </tr>
   	</thead>
   	<tbody>
@@ -31,9 +30,11 @@
           <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
         </div>
       </th>
-      <td>Sit</td>
-      <td>Amet</td>
-      <td>Consectetur</td>
+      <td>1</td>
+      <td>Julián</td>
+      <td>Mendez</td>
+      <td>56</td>
+      <td>1056</td>
     </tr>
     <tr>
       <th scope="row">
@@ -41,9 +42,11 @@
           <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
         </div>
       </th>
-      <td>Adipisicing</td>
-      <td>Elit</td>
-      <td>Sint</td>
+      <td>2</td>
+      <td>Pedro</td>
+      <td>Mendoza</td>
+      <td>32</td>
+      <td>800</td>
     </tr>
     <tr>
       <th scope="row">
@@ -51,23 +54,26 @@
           <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
         </div>
       </th>
-      <td>Hic</td>
-      <td>Fugiat</td>
-      <td>Temporibus</td>
+      <td>3</td>
+      <td>Augusto</td>
+      <td>Carmelo</td>
+      <td>21</td>
+
+      <td>348</td>
     </tr>
   	</tbody>
 	</table>
+	</div>
 
-	</div>
-	
-	<div class="btn-group" role="group" aria-label="Basic example">
-  		<button type="button" class="btn btn-primary">POST</button>
-  		<button type="button" class="btn btn-primary">GET</button>
-  		<button type="button" class="btn btn-primary">PUT</button>
-		<button type="button" class="btn btn-primary">PATCH</button>
-		<button type="button" class="btn btn-primary">DELETE</button>
-	</div>
-    </body>
+  <div class="btn-group" role="group" aria-label="Basic example">
+  <button type="button" class="btn btn-success">POST</button>
+  <button type="button" class="btn btn-success">GET</button>
+  <button type="button" class="btn btn-warning">PUT</button>
+  <button type="button" class="btn btn-warning">PATCH</button>
+  <button type="button" class="btn btn-danger">DELETE</button>
+
+  </div>
+  </body>
 </html>
 
 
